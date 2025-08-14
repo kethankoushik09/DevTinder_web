756bdad5f5fff2b8692e8e9facb372f6879e0f45
kethangaikwad <kethan@upgrowthmarketing.in>
2025-08-14 17:27:57 +0530

    added vercel file

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-import { BASE_URL } from "./constants";
+import { BASE_URL } from "./constants.jsx";
 
 const createSocketConnection = () => {
   return io(BASE_URL);
