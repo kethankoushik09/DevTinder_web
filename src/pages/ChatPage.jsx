34173d34487a82b0fb0014e517be88ad4a456dd0
kethangaikwad <kethan@upgrowthmarketing.in>
2025-08-15 19:52:10 +0530

    chat feature added

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -2,39 +2,67 @@ import React, { useEffect, useState } from "react";
 import createSocketConnection from "../utils/socket.js";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants.jsx";
 
 const ChatPage = () => {
   const { id } = useParams();
   const user = useSelector((state) => state.user);
-  const [message,SetMesaage] = useState("");
-  const [msgs,Setmsgs] =useState([]);
+  const [message, SetMesaage] = useState("");
+  const [msgs, Setmsgs] = useState([]);
   const userId = user?._id;
 
+  async function fetch_messages() {
+    try {
+      const chat = await axios.get(BASE_URL + "/chat/" + id, {
+        withCredentials: true,
+      });
+      console.log(chat.data.messages);
+      const chatmessages = chat.data.messages.map((itm) => {
+        const { senderId, text } = itm;
+        return {
+          firstName: senderId.firstName,
+          lastName: senderId.lastName,
+          text,
+        };
+      });
+      console.log(chatmessages);
+
+      Setmsgs(chatmessages);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
   useEffect(() => {
-    if(!userId){
+    fetch_messages();
+  }, []);
+  useEffect(() => {
+    if (!userId) {
       return;
     }
     const socket = createSocketConnection();
     socket.emit("joinchat", { userId, id });
 
-    socket.on("messageReceived",({firstName,text})=>{
-      console.log(firstName+"  "+text);
-      Setmsgs((prev) => [...prev,{firstName,text}]);
-      console.log("kk"+msgs);
-      
-    })
+    socket.on("messageReceived", ({ firstName, lastName, text }) => {
+      Setmsgs((prev) => [...prev, { firstName, lastName, text }]);
+    });
 
-    return ()=>{
+    return () => {
       socket.disconnect();
-    }
-  }, [userId,id]);
+    };
+  }, [userId, id]);
 
-
-  function sendMessage(){
+  function sendMessage() {
     const socket = createSocketConnection();
-    socket.emit("sendMessage",{firstName : user.firstName, userId, id ,text:message})
+    socket.emit("sendMessage", {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      userId,
+      toUserId: id,
+      text: message,
+    });
     SetMesaage("");
-
   }
 
   return (
@@ -45,28 +73,36 @@ const ChatPage = () => {
         <h1 className="text-2xl text-gray-400 border-b border-gray-100 p-2">
           chat
         </h1>
-        <div className="flex-1  p-5">
-          {msgs && msgs.length>0 && msgs.map((itm,idx) =>(
-             <div className="chat chat-start" key={idx}>
-            <div className="chat-header">
-              {itm.firstName}
-              <time className="text-xs opacity-50">2 hours ago</time>
-            </div>
-            <div className="chat-bubble">{itm.text}</div>
-            <div className="chat-footer opacity-50">Seen</div>
-          </div> 
-
-          ))}
-          
+        <div className="flex-1  p-5 overflow-y-scroll">
+          {msgs &&
+            msgs.length > 0 &&
+            msgs.map((itm, idx) => (
+              <div
+                key={idx}
+                className={
+                  "chat " +
+                  (user.firstName === itm.firstName ? "chat-end" : "chat-start")
+                }
+              >
+                <div className="chat-header">
+                  {itm.firstName} {itm.lastName}
+                  <time className="text-xs opacity-50">2 hours ago</time>
+                </div>
+                <div className="chat-bubble">{itm.text}</div>
+                <div className="chat-footer opacity-50">Seen</div>
+              </div>
+            ))}
         </div>
         <div className="flex gap-3 p-5 items-center">
           <input
             className="flex-1 p-2 border border-gray-500"
             placeholder="message..."
             value={message}
-            onChange={(e)=> SetMesaage(e.target.value)}
+            onChange={(e) => SetMesaage(e.target.value)}
           />
-          <button className="btn btn-secondary" onClick={sendMessage}>send</button>
+          <button className="btn btn-secondary" onClick={sendMessage}>
+            send
+          </button>
         </div>
       </div>
     </div>
